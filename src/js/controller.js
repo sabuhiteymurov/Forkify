c6ef3f2d44b4d762157c71620b1083d61aea9453
Sabuhi Teymurov <teymurov.sebuhi740@gmail.com>
2022-05-17 22:24:30 +0400

    0.1.0 - Implementing bookmarks

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 // CORRUPTS PARCEL
 import 'core-js/stable';
@@ -20,6 +21,7 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1) Loading recipe
     await model.loadRecipe(id);
@@ -69,10 +71,15 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // 1) Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.removeBookmark(model.state.recipe.id);
 
+  // 2) Update recipe view
   recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,4 +1,5 @@
 import View from './View.js';
+import previewView from './previewView.js';
 import icons from 'url:../../img/icons.svg';
 
 class ResultsView extends View {
@@ -7,30 +8,7 @@ class ResultsView extends View {
   _successMessage = '';
 
   _generateMarkup() {
-    return this._data
-      .map(d =>
-        this._generateMarkupPreview(d.id, d.image, d.title, d.publisher)
-      )
-      .join('');
-  }
-
-  _generateMarkupPreview(id, img, title, publisher) {
-    const wId = window.location.hash.slice(1);
-    return `
-      <li class="preview">
-        <a class="preview__link ${
-          id === wId ? 'preview__link--active' : ''
-        }" href="#${id}"> 
-          <figure class="preview__fig">
-            <img src="${img}" alt="${title}" />
-          </figure>
-          <div class="preview__data">
-            <h4 class="preview__title">${title}</h4>
-            <p class="preview__publisher">${publisher}</p>
-          </div>
-        </a>
-      </li>
-    `;
+    return this._data.map(result => previewView.render(result, false)).join('');
   }
 }
 
